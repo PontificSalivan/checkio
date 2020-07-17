33a544bb6f30560c256ffbcb8ceacd646ed70ab3
Arsen Manukyan <rasen13@mail.ru>
2020-07-17 17:45:22 +0400

    bigger_price

---
+++ bigger_price.py
@@ -0,0 +1,46 @@
+def bigger_price(limit: int, data: list) -> list:
+    
+    lst = list()
+    sets = []
+    answer = list()
+
+    for name in data:
+        sets = [name.get("price"),name.get("name")]
+        lst.append(sets)
+
+    lst.sort()
+
+    for i in range(limit):
+        d = {"name": lst[len(lst)-1-i][1], "price": lst[len(lst)-1-i][0]}
+        answer.append(d)
+    
+    return answer
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    print('Example:')
+    pprint(bigger_price(2, [
+        {"name": "bread", "price": 100},
+        {"name": "wine", "price": 138},
+        {"name": "meat", "price": 15},
+        {"name": "water", "price": 1}
+    ]))
+
+    # These "asserts" using for self-checking and not for auto-testing
+    assert bigger_price(2, [
+        {"name": "bread", "price": 100},
+        {"name": "wine", "price": 138},
+        {"name": "meat", "price": 15},
+        {"name": "water", "price": 1}
+    ]) == [
+        {"name": "wine", "price": 138},
+        {"name": "bread", "price": 100}
+    ], "First"
+
+    assert bigger_price(1, [
+        {"name": "pen", "price": 5},
+        {"name": "whiteboard", "price": 170}
+    ]) == [{"name": "whiteboard", "price": 170}], "Second"
+
+    print('Done! Looks like it is fine. Go and check it')
